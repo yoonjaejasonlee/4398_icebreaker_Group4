b9d6f1eadf8fe0c92348cebdd9f7ad80c1814166
Vivek R Patel <111992734+vivekpatell3@users.noreply.github.com>
2023-08-29 20:57:41 -0700

    Testing

---
+++ config.py
@@ -0,0 +1,5 @@
+AUTH_KEY  = "Bearer wj1MEuhIgxcFDp9PnML9E3elUMZwXkttWKUmA7Lk"
+
+PIC = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Temple_T_logo.svg/905px-Temple_T_logo.svg.png"
+
+BOT_TOKEN = "MTE0NTgwNTY3ODAzMjcyMDA1Mg.GUGtwn.wpmGIcojGl18mJxlXRkRsXmEFYU6TGrG0ff_Zs"
